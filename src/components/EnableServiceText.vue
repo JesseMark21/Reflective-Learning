2f4d71f1919f33110a74907f0c3f02fd846b27a0
Mr-SlR <jessemark21@gmail.com>
2020-12-11 12:25:25 +0200

    Checkboxes input capture for enable service

--- src/components/EnableServiceText.vue
+++ src/components/EnableServiceText.vue
@@ -1,17 +1,33 @@
 <template>
-	<h2>Enable Service</h2>
-	<input type="checkbox" id="sms" value="SMS Saver" v-model="checkedNames">
+	<h2>{{heading}}</h2>
+	<input type="checkbox" id="sms" value="SMS Saver" v-model="checkedNames" @change="smsChecked($event)">
 	<label for="sms">SMS Saver</label>
-	<input type="checkbox" id="url" value="URL Shortening" v-model="checkedNames">
+	<input type="checkbox" id="url" value="URL Shortening" v-model="checkedNames" @change="urlChecked($event)">
 	<label for="url">URL Shortening</label>
+
 </template>
 
 <script>
 	export default {
     data(){
       return{
+        heading: "Enable Service",
+        checkedNames:[],
       }
-    }
+    },
+    methods : {
+      urlChecked : function(value) {
+        this.checkedNames[0] = !this.checkedNames[0];
+        this.checkedNames.push();
+        this.heading = "url";
+      },
+      smsChecked : function(value) {
+        this.checkedNames[1] = !this.checkedNames[1];
+        this.checkedNames.push();
+        this.heading = "sms";
+      }
+    },
+    
   }
 </script>
 <style>
