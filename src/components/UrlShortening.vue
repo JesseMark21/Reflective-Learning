f32944c8fac764029541fcb147d62bc2d803aa9a
Mr-SlR <jessemark21@gmail.com>
2020-12-11 15:53:03 +0200

    1 file modified

--- src/components/UrlShortening.vue
+++ src/components/UrlShortening.vue
@@ -1,10 +1,10 @@
 <template>
 	<h2>URL Shortening</h2>
 	<div class="e-float-input e-input-group">
-		<input type="text" v-model="pText" required/>
+		<input type="text" v-model="urlInput" required/>
 		<span class="e-float-line"></span>
 		<label class="e-float-text"> Enter the URL to be shortened </label>
-		<p> URL: {{pText}}</p>
+		<p> URL: {{urlInput}}</p>
 	</div>
 </template>
 
@@ -12,7 +12,7 @@
 	export default {
     data(){
       return{
-        pText:''
+        urlInput:''
       }
     }
   }
