f1c221207d34cc8afb4d1ffa49e2503ffbded298
Mr-SlR <jessemark21@gmail.com>
2020-12-11 14:12:36 +0200

    URL Shortening added

--- src/components/EnableServiceText.vue
+++ src/components/EnableServiceText.vue
@@ -4,49 +4,57 @@
 	<label for="sms">SMS Saver</label>
 	<input type="checkbox" id="url" value="URL Shortening" v-model="checkedNames" @change="urlChecked($event)">
 	<label for="url">URL Shortening</label>
-	<h3> {{selected}} </h3>
+	<h3> Selected: {{selected}} </h3>
+	<div v-if=selected==="url">
+		<url-shortening></url-shortening>
+		</v-if>
+	</div>
 
 </template>
 
 <script>
+	import UrlShortening from './UrlShortening.vue'
 	export default {
     data(){
       return{
         heading: "Enable Service",
         checkedNames:[false, false],
-        selected: "Selected: none",
+        selected: "none",
       }
+    },
+    components:{
+      'url-shortening':UrlShortening,
     },
     methods : {
       urlChecked : function(value) {
         this.checkedNames[0] = !this.checkedNames[0];
         if(this.checkedNames[0] && this.checkedNames[1]){
-          this.selected = "Selected: both";
+          this.selected = "both";
         }
         else if(this.checkedNames[0]){
-          this.selected = "Selected: url";
+          this.selected = "url";
         }
         else if(this.checkedNames[1]){
-          this.selected = "Selected: sms";
+          this.selected = "sms";
         }
         else if (!this.checkedNames[0] && !this.checkedNames[1]){
-          this.selected = "Selected: none";
+          this.selected = "none";
         }
         
       },
       smsChecked : function(value) {
         this.checkedNames[1] = !this.checkedNames[1];
         if(this.checkedNames[0] && this.checkedNames[1]){
-          this.selected = "Selected: both";
+          this.selected = "both";
         }
         else if(this.checkedNames[1]){
-          this.selected = "Selected: sms";
+          this.selected = "sms";
         }
         else if(this.checkedNames[0]){
-          this.selected = "Selected: url";
+          this.selected = "url";
         }
         else if (!this.checkedNames[0] && !this.checkedNames[1]){
-          this.selected = "Selected: none";
+          this.selected = "none";
         }
       }
     },
---
+++ src/components/UrlShortening.vue
@@ -0,0 +1,29 @@
+<template>
+	<h2>URL Shortening</h2>
+	<div class="e-float-input e-input-group">
+		<input type="text" v-model="pText" required/>
+		<span class="e-float-line"></span>
+		<label class="e-float-text"> Enter the URL to be shortened </label>
+		<p> URL: {{pText}}</p>
+	</div>
+</template>
+
+<script>
+	export default {
+    data(){
+      return{
+        pText:''
+      }
+    }
+  }
+</script>
+<style>
+	e-float-input e-input-group {
+		padding-top: 0px;
+	}
+
+	h2 {
+		color: #000000;
+		text-align: left;
+	}
+</style>
